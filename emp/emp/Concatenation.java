ffe1b230427c026266cdb9ee6f25079deb498b05
Ashok <ashokkumarm0007@gmail.com>
2019-08-28 14:26:49 -0700

    ready to push

---
+++ emp/emp/Concatenation.java
@@ -0,0 +1,18 @@
+package com.Rogersoft.javaprograms.printconcat;
+
+import java.util.Scanner;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner (System.in);
+			String s = sc.next();
+			String a = sc.next();
+			System.out.println(s+a);
+			
+			
+			
+
+	}
+
+}
